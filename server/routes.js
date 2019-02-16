78090157346859a07de193d50ceff523d20ab22a
geekhijabi <bimpeadetunji@rocketmail.com>
2019-02-17 00:34:09 +0100

    create server controller for generating random number and write test

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,15 @@
+import { create } from "domain";
+import randomphoneController from "./randomphone.controller";
+
 const baseUrl = '/api/v1';
 
 const routes = (app) => {
   app.get(`${baseUrl}`, (req, res) => 
   res.status(200).send('Welcome to Random Phone Number Generator!')
 )
+
+app.get(`${baseUrl}/create-phonenumbers`, randomphoneController.createRandomNumbers);
+app.get(`${baseUrl}/get-phonenumbers`, randomphoneController.getRandomNumbers);
 }
 
 export default routes;
--- server/test.spec.js
+++ server/test.spec.js
@@ -6,12 +6,28 @@ import server from '../server';
 chai.should();
 chai.use(chaiHttp);
 
-describe('RPNG', () => {
+describe('Random Phonenumber Generator', () => {
   it('should display the welcome page', (done) => {
     chai.request(server).get('/api/v1')
         .end((err, res) => {
           res.should.have.status(200);
           done();
+        });
+  });
+
+  it('should generate random number', () => {
+    chai.request(server).get('/api/v1/create-phonenumbers')
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.message.should.equal('Numbers successfully generated')
+        });
+  });
+
+  it('should get all random numbers', () => {
+    chai.request(server).get('/api/v1/get-phonenumbers')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.equal('Numbers fetched successfully')
         });
   });
 });
